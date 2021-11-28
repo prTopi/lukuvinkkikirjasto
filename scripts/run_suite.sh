82f0f2ac5fb06fcf0be43f000be8504fe19ee811
Topi Harjunpää <topi@harjunpaa.fi>
2021-11-28 16:08:44 +0200

    fix selenium tests action

--- scripts/run_suite.sh
+++ scripts/run_suite.sh
@@ -6,6 +6,9 @@ if [ -z "$TEST_SUITE" ]; then
     TEST_SUITE=""
 fi
 
+# Give the app time to start
+sleep 5
+
 CMD="robot --console verbose --outputdir /reports /src/tests/$TEST_SUITE"
 
 echo ${CMD}
