066c33d880cb6f7a6223d8a5ce94690bf652bdd6
Topi Harjunpää <topi@harjunpaa.fi>
2021-12-01 15:09:25 +0200

    add user login and user creation capability

--- src/db.py
+++ src/db.py
@@ -9,6 +9,22 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 
+def find_user_id(username):
+    sql = "SELECT id FROM Users WHERE username=:username"
+    try:
+        return db.session.execute(sql, {"username":username}).fetchone()[0]
+    except:
+        return None
+
+def find_password(username):
+    sql = "SELECT password FROM Users WHERE username=:username"
+    return db.session.execute(sql, {"username":username}).fetchone()
+
+def insert_user(username,password):
+    db.session.execute("INSERT INTO Users (username, password) VALUES (:username, :password)", {"username":username,"password":password})
+    db.session.commit()
+
+
 def insert_bookmark(user_id, description):
     sql = """
     INSERT INTO Bookmarks
--- src/routes.py
+++ src/routes.py
@@ -1,18 +1,65 @@
 """Module containing all routes of the server"""
 from app import app
-from flask import redirect, render_template, request
+from flask import redirect, render_template, request, session, abort
+from werkzeug.security import check_password_hash, generate_password_hash
 from os import getenv
+from secrets import token_hex
 import db
 
 app.secret_key = getenv("SECRET")
 
-user_id = 1
-
 
 @app.route("/")
 def index():
-    return render_template("index.html", books=db.get_all_books(1))
+    try:
+        books = db.get_all_books(session["user_id"])
+    except KeyError:
+        books = None
+    return render_template("index.html", books=books)
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/log", methods=["POST"])
+def log():
+    username = request.form["username"]
+    password = request.form["password"]
+    hash_value = db.find_password(username)
+    if hash_value != None:
+        if check_password_hash(hash_value[0],password):
+            session["user_id"] = db.find_user_id(username)
+            session["username"] = username
+            session["csrf_token"] = token_hex(16)
+            return redirect("/")
+    return redirect("/login")
+
+@app.route("/logout")
+def logout():
+    del session["user_id"]
+    del session["username"]
+    del session["csrf_token"]
+    return redirect("/")
+
+@app.route("/create")
+def create():
+    return render_template("create.html",error=False)
+
+@app.route("/create_account", methods=["POST"])
+def create_account():
+    username = request.form["username"]
+    password = request.form["password"]
+    password2 = request.form["password2"]
+
+    user_id = db.find_user_id(username)
+    if user_id != None:
+        return render_template("create.html", error="Username taken", user=username)
+    if password != password2:
+        return render_template("create.html", error="Passwords not identical", user=username)
+
+    password = generate_password_hash(password2)
+    db.insert_user(username,password)
+    return redirect("/log")
 
 @app.route("/add_bookmark")
 def add_bookmark():
@@ -21,16 +68,17 @@ def add_bookmark():
 
 @app.route("/add", methods=["POST"])
 def add():
+    if session["csrf_token"] != request.form["csrf_token"]:
+        abort(403)
     book_type = request.form["type"]
     title = request.form["title"]
     description = request.form["description"]
     author = request.form["author"]
     if book_type == "book":
-
         isbn = request.form["ISBN"]
-        db.insert_book(user_id, title, description, author, isbn)
+        db.insert_book(session["user_id"], title, description, author, isbn)
     elif book_type == "video":
         link = request.form["link"]
-        db.insert_video(user_id, title, description, author, link)
+        db.insert_video(session["user_id"], title, description, author, link)
 
     return redirect("/")
