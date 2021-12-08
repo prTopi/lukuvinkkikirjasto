7de782f55f4d9f67f97f457b0df16c26ef5f509d
Tuomas Alanen <tualanen@cs.helsinki.fi>
2021-12-08 16:31:50 +0200

    worked with releasing actions

--- tasks.py
+++ tasks.py
@@ -10,6 +10,17 @@ def start(ctx):
 def pylint(ctx):
     ctx.run('pylint src')
 
+
 @task
 def test(ctx):
     ctx.run('pytest src')
+
+
+@task
+def coverage(ctx):
+    ctx.run('coverage run -m pytest src')
+
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html -d documentation/test_reports/unittests")
